05c8e53fb060d9ecc90b072421ecc6ac0f49b9a8
Aguang <aguang.xyz@gmail.com>
2020-08-03 15:22:16 +1200

    Upgrade.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="rnz_news",
-    version="0.0.3",
+    version="0.0.4",
     author="Guangrui Wang",
     author_email="aguang.xyz@gmail.com",
     description="Retrieve RNZ news.",
