b4d768016958e558899171a4ef1fd98eaca6d21d
Guangrui Wang <aguang.xyz@gmail.com>
2020-09-05 21:32:18 +1200

    Reformat code.

--- dotnet/RnzNewsTest/RnzNewsTest.cs
+++ dotnet/RnzNewsTest/RnzNewsTest.cs
@@ -11,49 +11,50 @@ namespace RnzNewsTest
         [TestMethod]
         public void TestCategories()
         {
-          var client = new RnzNewsClient();
-          
-          Assert.IsTrue(client.Categories.Count() > 0);
+            var client = new RnzNewsClient();
 
-          foreach (var category in client.Categories) {
+            Assert.IsTrue(client.Categories.Count() > 0);
 
-            Console.WriteLine("Path: {0}", category.Path);
-            Console.WriteLine("Address: {0}", category.Address);
-            Console.WriteLine("Description: {0}", category.Description);
-          }
+            foreach (var category in client.Categories)
+            {
+
+                Console.WriteLine("Path: {0}", category.Path);
+                Console.WriteLine("Address: {0}", category.Address);
+                Console.WriteLine("Description: {0}", category.Description);
+            }
         }
 
         [TestMethod]
         public void TestAllArticles()
         {
-          var client = new RnzNewsClient();
-          var sportCategory = client["news/sport"];
+            var client = new RnzNewsClient();
+            var sportCategory = client["news/sport"];
 
-          Assert.IsNotNull(sportCategory != null);
+            Assert.IsNotNull(sportCategory != null);
 
-          foreach (var chunk in sportCategory)
-          {
-            var articles = chunk.Item1;
-            var page = chunk.Item2;
-
-            Console.WriteLine("Page: {0}", page);
-
-            foreach (var article in articles)
+            foreach (var chunk in sportCategory)
             {
-              Console.WriteLine("Category: {0}", article.Category);
-              Console.WriteLine("Path: {0}", article.Path);
-              Console.WriteLine("Address: {0}", article.Address);
-              Console.WriteLine("Title: {0}", article.Title);
-              Console.WriteLine("Summary: {0}", article.Summary);
-              Console.WriteLine("Time: {0}", article.Time);
-              Console.WriteLine("Content: {0}", article.Content);
-              Console.WriteLine("Html: {0}", article.Html);
+                var articles = chunk.Item1;
+                var page = chunk.Item2;
 
-              break;
+                Console.WriteLine("Page: {0}", page);
+
+                foreach (var article in articles)
+                {
+                    Console.WriteLine("Category: {0}", article.Category);
+                    Console.WriteLine("Path: {0}", article.Path);
+                    Console.WriteLine("Address: {0}", article.Address);
+                    Console.WriteLine("Title: {0}", article.Title);
+                    Console.WriteLine("Summary: {0}", article.Summary);
+                    Console.WriteLine("Time: {0}", article.Time);
+                    Console.WriteLine("Content: {0}", article.Content);
+                    Console.WriteLine("Html: {0}", article.Html);
+
+                    break;
+                }
+
+                break;
             }
-
-            break;
-          }
         }
     }
 } // namespace RnzNewsTest
